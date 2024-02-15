69a236a20fcec1de2a335ac9e800f8daccc316a4
Prerna Kumari <126183593+Prernna@users.noreply.github.com>
2024-02-16 00:03:20 +0530

    Create constraint.sql
    /* Write a query to create a table employee with the mentioned constraints on the columns : 
    employee_id - PRIMARY KEY, 
    employee_Name -UNIQUE, 
    Department -NOT NULL */

---
+++ constraint.sql
@@ -0,0 +1,17 @@
+Write a query to create a table employee with the mentioned constraints on the columns :
+
+employee_id - INTEGER PRIMARY KEY,
+employee_Name - TEXT UNIQUE,
+Department - TEXT NOT NULL
+
+SOLUTION:
+
+
+Create table employee(
+
+employee_id integer primary key,
+employee_Name text unique ,
+Department text not null
+);
+
+
